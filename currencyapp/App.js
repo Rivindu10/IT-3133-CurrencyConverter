c7880e3238be1daa11f9d882d86bc128239a7b6d
Rivindu_Pabasara <101160321+Rivindu10@users.noreply.github.com>
2024-12-29 21:44:19 +0530

    create currency converter

--- currencyapp/App.js
+++ currencyapp/App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Alert,StyleSheet } from 'react-native';
+import { Button, TextInput } from 'react-native-web';
 
 export default function App() {
+ const [lkrAmount, setLkrAmount]=useState('');
+ const [usdAmount, setUsdAmount]=useState(null);
+
+ const convertRate=0.003;
+
+ const converSion = ()=>{
+  const amount = parseFloat(lkrAmount);
+  if (isNaN(amount) || amount<=0){
+    Alert.alert('Invalid Input','Please enter a valid amount in LKR');
+    return;
+  }
+  const convertedAmount =(amount * convertRate).toFixed(2);
+  setUsdAmount(convertedAmount);
+ }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>LKR to USD Converter</Text>
+      <TextInput
+
+      placeholder='Enter amount in LKR'
+      keyboardType='numeric'
+      value={lkrAmount}
+      onChangeText={setLkrAmount}
+      
+      />
+
+      <Button title="Convert" onPress={converSion}/>
+      {usdAmount && (
+        <Text style={styles.result}>USD: ${usdAmount}</Text>
+      )}
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
